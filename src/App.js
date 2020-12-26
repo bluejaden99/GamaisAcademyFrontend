1770083ffb1521e5afe3beefab928c74e9824be7
desyasyasha <13517037@std.stei.itb.ac.id>
2020-12-26 23:19:59 +0700

    navbar done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import VideoPlayer from './components/VideoPlayer';
 import PDFViewer from './components/PDFViewer';
-import Footer from './components/footer'
+import Footer from './components/footer';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <VideoPlayer videoName="PAT_18_11_2020.mp4"/>
         <PDFViewer documentName="probstat-book.pdf"/>
---
+++ src/components/navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import logo from '../assets/img/logo192.png';
+import { withStyles } from '@material-ui/core/styles';
+import './navbar.css'
+import './footer.css'
+
+const NavbarButton = withStyles({
+  root: {
+    textTransform: 'none',
+    fontSize: '16px',
+    width : '100px',
+    borderRadius : '10px',
+    margin : '5px 0px',
+    color : "white",
+    backgroundColor :"#E3B448",
+    '&:hover': {
+      backgroundColor: "#E3B448",
+      opacity : "90%",
+    }
+  },
+})(Button);
+
+class Navbar extends React.Component {
+  render(){
+    return(
+      <div >
+        <AppBar position="static" color="inherit">
+          <Toolbar id = "navbarContainer">
+            <div id = "rowContainer">
+              <IconButton >
+                <img src= {logo} className = "navbarLogo"></img>
+              </IconButton>
+              <Typography variant="h6" > Gamais Academy</Typography>
+            </div>
+            <div>
+              <NavbarButton>Login</NavbarButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+  }
+}
+  
+export default Navbar
