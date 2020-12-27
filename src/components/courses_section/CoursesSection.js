eb2afc5c4355727d5794f56befc81cc6b6f1143b
Aditya Bimawan <aditditto@gmail.com>
2020-12-28 06:14:25 +0700

    carousel courses

--- src/components/courses_section/CoursesSection.js
+++ src/components/courses_section/CoursesSection.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import CourseCard from '../course_card/CourseCard';
+import Slider from "react-slick";
 import styles from './CoursesSection.module.css';
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 const CoursesSection = ({ courses }) => {
     return (
         <div>
             <h2 className={ styles.myCourses }>My Courses</h2>
             <div className={ styles.coursesContainer }>
-                {
-                    courses.map((course) => {
-                        return <CourseCard {...course}/>
-                    })
-                }
+                <Slider
+                arrows={true}
+                dots={true}
+                draggable={false}
+                slidesToShow={3}
+                slidesToScroll={3}
+                >
+                    {
+                        courses.map((course) => {
+                            return <CourseCard {...course} key={course.title}/>
+                        })
+                    }
+                </Slider>
             </div>
         </div>
     )
